d1aeffd8b57ed7641ce8df82a6733074c0f1b8fd
Bisser Todorov <bisser.todorov.2003@gmail.com>
2025-01-19 14:19:33 +0200

    Progress ...

--- src/Service/Manager/TopicManager.php
+++ src/Service/Manager/TopicManager.php
@@ -36,7 +36,7 @@ class TopicManager
         $hours = 0;
         $resources = $topic->getResources();
 
-dump($resources);
+dd($resources);
         $children = $this->entityManager->getRepository(Topic::class)->findBy(['parent' => $topic->getId()]);
         dump($children);
         if (empty($children)) {
