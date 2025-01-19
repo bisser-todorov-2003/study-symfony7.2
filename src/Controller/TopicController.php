358a3a7e48aee321c9b93c662220b95402dfa571
Bisser Todorov <bisser.todorov.2003@gmail.com>
2025-01-19 11:07:27 +0200

    Progress ...

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -36,7 +36,6 @@ class TopicController extends AbstractController
     public function list(int $parent): Response
     {
         $topics = $this->topicManager->getTopicsByParentId($parent);
-        dd($topics);
         return $this->render('topic/index.html.twig', [
             'topics' => $topics,
         ]);
--- src/Service/Manager/TopicManager.php
+++ src/Service/Manager/TopicManager.php
@@ -4,16 +4,24 @@ namespace App\Service\Manager;
 
 use App\Entity\Topic;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class TopicManager
 {
-    public function __construct(private EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly SerializerInterface $serializer
+    )
     {}
 
     public function getTopicsByParentId(?int $parent): array
     {
-        $topics = [];
+        $result = [];
         $topics = $this->entityManager->getRepository(Topic::class)->findBy(['parent' => $parent]);
-        return $topics;
+        foreach ($topics as $topic) {
+            $dto = $this->serializer->denormalize($topic, Topic::class);
+            $result[] = $dto;
+        }
+        return $result;
     }
 }
