289276e20ff26af7f03d7efa5329f9770fbb0c02
Bisser Todorov <bisser.todorov.2003@gmail.com>
2025-01-28 21:35:08 +0200

    ProgressLog

---
+++ src/Entity/ProgressLog.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProgressLogRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProgressLogRepository::class)]
+class ProgressLog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $resourceId = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $start = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $finish = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResourceId(): ?int
+    {
+        return $this->resourceId;
+    }
+
+    public function setResourceId(int $resourceId): static
+    {
+        $this->resourceId = $resourceId;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeInterface $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getFinish(): ?\DateTimeInterface
+    {
+        return $this->finish;
+    }
+
+    public function setFinish(\DateTimeInterface $finish): static
+    {
+        $this->finish = $finish;
+
+        return $this;
+    }
+}
